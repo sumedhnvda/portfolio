e3b5a3ddc5d4b04657ba7dddf42183bf00b21090
Sumedh Navuda <sumedhnavuda@outlook.com>
2025-06-09 14:38:30 +0000

    blog added

--- src/components/sections/SkillsMarquee/skillsData.ts
+++ src/components/sections/SkillsMarquee/skillsData.ts
@@ -1,29 +1,5 @@
 export const skills = [
   {
-    name: 'C',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg'
-  },
-  {
-    name: 'Java',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg'
-  },
-  {
-    name: 'JavaScript',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg'
-  },
-  {
-    name: 'TypeScript',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg'
-  },
-  {
-    name: 'React',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'
-  },
-  {
-    name: 'Node.js',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg'
-  },
-  {
     name: 'Python',
     icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg'
   },
--- src/data/blogs.ts
+++ src/data/blogs.ts
@@ -1,13 +1,14 @@
 import type { BlogPost } from '../types';
 
 export const blogs: BlogPost[] = [
-  /*{
-    id: '1',
-    title: '',
-    date: '',
-    description: '',
-    url: '',
-    readTime: '',
-    tags: []
-  },*/
+{
+  id: '1',
+  title: 'Git Essentials: A Quick Cheat Sheet',
+  date: '09=06-2025',
+  description: 'A concise Git cheat sheet covering essential commands for everyday developers. Ideal for quick reference and beginners looking to get started with version control.',
+  url: 'https://medium.com/@sumedhnavuda/git-essentials-4b9acd65f858',
+  readTime: '2 min read',
+  tags: ['Git', 'Version Control', 'Cheat Sheet', 'Beginner']
+}
+
 ];
