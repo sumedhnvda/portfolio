73de1a71347579ee24e3aefbfd92faf269f2ee80
Sumedh Navuda <sumedhnavuda@outlook.com>
2025-06-29 03:16:53 +0000

    job role

--- src/data/workExperience.ts
+++ src/data/workExperience.ts
@@ -12,5 +12,18 @@ export const workExperience: WorkExperience[] = [
       'Built a Work division within the club to build and deliver projects for the college and local community',
     ],
     technologies: ['Leadership', 'Event Management', 'Public Speaking']
+  },
+  {
+    id: '2',
+    company: 'SangamOne Connected Services Pvt Ltd',
+    role: 'Intern',
+    period: '2025 - Present',
+    description: 'Worked on a product where I led the team, guided them through development, and conducted teaching sessions internally and externally.',
+    achievements: [
+      'Led a product team from planning to delivery',
+      'Mentored and taught peers regularly as part of internal learning sessions',
+      'Conducted external knowledge-sharing sessions as part of the organization’s outreach'
+    ],
+    technologies: ['Team Leadership', 'Teaching', 'Community Engagement']
   }
-];
+]
